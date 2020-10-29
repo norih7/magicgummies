5c821d0051b26301e7caefbc98defb5afc07a48c
norih <>
2020-10-29 20:47:40 +0900

    initial commit

---
+++ pages/hoge/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Hoge = () => {
+  return (
+    <>
+      hoge
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Hoge;
